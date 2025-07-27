42ec31007937f989122e00048ddc4ac18062546b
icegrave0391 <zhangchuqi1999@gmail.com>
2025-07-27 16:02:02 -0700

    cleanup

--- workloads/superuser-checkcollection.sh
+++ workloads/superuser-checkcollection.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# 获取所有包含file字段的collection及其file字段名
+# 获取所有collections名称
 curl -s http://localhost:8090/api/collections \
   -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xsZWN0aW9uSWQiOiJwYmNfMzE0MjYzNTgyMyIsImV4cCI6MTc1MzY4MjIzMywiaWQiOiJ4b2FnOWkxYWlvOGlvNHgiLCJyZWZyZXNoYWJsZSI6dHJ1ZSwidHlwZSI6ImF1dGgifQ.xKIV2SjxKYoCfe4jiwWuGIC5EN97K0L5zNzybB-svJs" \
-  | jq -r '
-    .items[]
-    | select(.fields[]?.type=="file")
-    | .name as $c
-    | .fields[] | select(.type=="file") | "\($c)\t\(.name)"
-  '
+  | jq -r '.items[].name'
