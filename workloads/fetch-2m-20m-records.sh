ec863203b9519df30fc66d3cc99c30898e6e0831
icegrave0391 <zhangchuqi1999@gmail.com>
2025-07-29 00:09:33 -0700

    x

--- workloads/fetch-2m-20m-records.sh
+++ workloads/fetch-2m-20m-records.sh
@@ -5,7 +5,7 @@ CLIENTS=1
 
 # /api/files/<collectionName>/<recordId>/<filename>
 FILE_2M_API="api/files/collection_files/333333333333333/file_2_mb_6bc7wxbwn1.txt"
-FILE_5M_API="api/files/collection_files/000000000000000/file_20_mb_ok77l01l2a.txt"
+FILE_20M_API="api/files/collection_files/000000000000000/file_20_mb_ok77l01l2a.txt"
 TOKEN=""
 
 
@@ -13,4 +13,4 @@ TOKEN=""
 ab -n $NUM_REQ -c $CLIENTS localhost:8090/${FILE_2M_API}?token=$TOKEN
 
 # request 20MB file 20 times
-ab -n $NUM_REQ -c $CLIENTS localhost:8090/${FILE_5M_API}?token=$TOKEN
+ab -n $NUM_REQ -c $CLIENTS localhost:8090/${FILE_20M_API}?token=$TOKEN
